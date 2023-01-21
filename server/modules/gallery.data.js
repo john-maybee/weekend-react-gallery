f4fbfc4878ddeab3e650cd59fb3007a4607030ec
John Maybee <john.d.maybee@gmail.com>
2023-01-20 20:02:32 -0500

    added a bunch of jpeg files to the images folder. Also added them to the galleryItems array in the modules.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/83071B71-BA1B-4723-A637-A3F3B60039CD_1_105_c.jpeg', description: 'Photo of Winnie and toy.', likes: 0 },
+    { id: 2, path: 'images/DE90A8C3-E037-45A7-A3B6-7F1E60FB6CA9_1_105_c.jpeg', description: 'Photo of Roux eating.', likes: 0 },
+    { id: 3, path: 'images/96CDD449-A9F0-44F1-BC56-CF3A74C7D4EA_1_105_c.jpeg', description: 'Photo of Grandpa Hank the pug.', likes: 0 },
+    { id: 4, path: 'images/93FC1495-BF8B-4F40-861C-7DD6782D8250_1_105_c.jpeg', description: 'Photo of vacation in Monaco.', likes: 0 },
+    { id: 5, path: 'images/CAAD4967-9F59-42CD-AA4F-E8CBC914AAF9_1_105_c.jpeg', description: 'Photo of dinner in Cannes.', likes: 0 },
+    { id: 6, path: 'images/930F1425-550B-40B6-9EEC-75241595466B_1_105_c.jpeg', description: 'Photo of woodworking magazine wall.', likes: 0 }
 ];
 
 
 module.exports = galleryItems;
+

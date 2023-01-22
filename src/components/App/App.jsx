eb93b81bef79c685acf946e2dbba4d13c0bd71f3
John Maybee <john.d.maybee@gmail.com>
2023-01-21 21:45:15 -0500

    added the axios.get to the app.jsx. Also, triggered that by using useEffect which I need to import after this commit. Also added a few more imports to the app.jsx and added the list const

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import GalleryList from '../GalleryList/GalleryList';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  const fetchData = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+    .then( (response) => {
+      console.log('Response from /gallery: ', response);
+      setGalleryList(response.data);
+    })
+    .catch(function (error) {
+      console.log('Error on GET: ', error);
+    });
+  }
+
+  useEffect( () => {
+    fetchData();
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <img src="images/goat_small.jpg"/>
+    </div>
+  );
 }
 
 export default App;

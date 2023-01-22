59e37b7ba66c14d3c4a79a17b653ece10ae8b8b6
John Maybee <john.d.maybee@gmail.com>
2023-01-22 17:12:12 -0500

    decided to add a <p> to hold the object.description within the GalleryItem.jsx. This way I can see what i'll be working on when swapping the image with description.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,6 +4,7 @@ function GalleryItem({object}){
             <div className="object" key={object.id}>
             <h3>{object.title}</h3>
             <img src={object.path}/>
+            <p>{object.description}</p>
             {/* {imgDescriptionSwap} */}
             <br/>
             <button>love it!</button>
